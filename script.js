57f5d5049cdf5bd7671180b37094ca051822b11f
Alan Soares <103722664+Cruelsoldies@users.noreply.github.com>
2023-06-26 12:06:19 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMode() {
+  let html = document.documentElement
+  //pega a tag toggle e coloca o light-mode
+  html.classList.toggle('light-mode')
+
+  //if(html.classList.contain('light-mode'))
+  
+  
+}
